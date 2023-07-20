c3e9ee3c370c5bed96b3d3e1908a1a88b4015511
Kwentsir <kwentsirkwesi@gmail.com>
2023-07-20 15:56:17 +0000

    Setup react folder structure

--- book/views.py
+++ book/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-@api_view(['GET'])
-def get_all_books(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
